6de680d3b56f8279a72fbdc2a452c55f0070c43a
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2025-04-30 13:16:18 +0530

    Create main.dart

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+void main() => runApp(
+      DevicePreview(
+        builder: (context) => MyApp(),
+      ),
+    );
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      title: 'Flutter App',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter App"),
+      ),
+      body: Container(
+        // margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(20),
+        height: 200,
+        width: double.infinity,
+        color: Colors.blue,
+        child: Text('Flutter App'),
+      ),
+    );
+  }
+}
