b57ec59c5dc1ffd90869d3ca537e429f9cf1bd3e
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2025-05-01 01:19:47 +0530

    Update main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter_application_1/screens/homepage.dart';
+import 'package:flutter_application_1/screens/secondscreen.dart';
 
 void main() => runApp(
       DevicePreview(
@@ -7,59 +9,21 @@ void main() => runApp(
       ),
     );
 
-class MyApp extends StatelessWidget {  // ✅ Correct
+class MyApp extends StatelessWidget {
+  // ✅ Correct
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter App',
       home: HomePage(),
+      routes: <String, WidgetBuilder>{
+        '/second': (context) => Secondscreen(),
+      },
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-    int counter = 0;  // ✅ fixed spelling
-
-  void incrementCounter() {
-    counter++;
-    print(counter);  // ✅ fixed spelling
-  }
-    @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter App"),
-        backgroundColor: const Color.fromARGB(255, 10, 152, 247),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Push Up",
-              style: TextStyle(fontSize: 20),
-            ),
-            Text(
-              counter.toString(),
-              style: Theme.of(context).textTheme.displayLarge,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: incrementCounter,  // ✅ fixed spelling
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
 
 // class HomePage extends StatelessWidget {
 
